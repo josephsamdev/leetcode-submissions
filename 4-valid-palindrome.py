91b897742deffcdacc2bdc825a23caadf2c87791
Joseph Sam <kobisam02@gmail.com>
2026-08-18 14:21:02 +0100

    Adding second solution for Valid Palindrome - this is a O(n) solution

--- 4-valid-palindrome.py
+++ 4-valid-palindrome.py
@@ -8,3 +8,26 @@ class Solution:
         return newStr == newStr[::-1]
 
 
+# Here's the O(n) solution:
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        n = len(s)
+        L = 0
+        R = n -1 
+
+        while L < R:
+            if not s[L].isalnum():
+                l += 1
+                continue
+
+            if not s[R].isalnum():
+                R -= 1
+                continue
+
+            if s[L].lower() != s[R].lower():
+                return False 
+
+            L += 1
+            R -= 1
+
+        return True 
